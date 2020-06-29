ca76ab6333fb54a99ced0777c3256c719a4252fb
ahmed_mansour <ahmed.mansour@code95.info>
2020-06-29 08:16:04 +0200

    render burger to the frontend && structure build contorl

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/Auxiliary'
-
+import Burger from '../../components/Burger/Burger'
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 0,
+
+    }
+  }
   render() {
     return (
       <Aux>
-        <div>
-          Burger
-        </div>
+        <Burger ingredients={this.state.ingredients} />
         <div>
           Build Controls
         </div>
