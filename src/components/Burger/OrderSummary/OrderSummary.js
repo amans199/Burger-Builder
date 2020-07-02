df5a07bf08906f45f63fb534f7812616bf6581ef
amans199 <ahmed.ouda1997@gmail.com>
2020-07-02 20:52:12 +0200

    improve performance && change the files and folders structure

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import Aux from '../../../hoc/Auxiliary'
+import React, { Component } from 'react';
+import Aux from '../../../hoc/Auxiliary/Auxiliary'
 import Button from '../../UI/Button/Button'
-const orderSummary = (props) => {
-  const ingredientsSummary = Object.keys(props.ingredients).map(igkey => {
-    return <li key={igkey}><span style={{ textTransform: 'capitalize' }}>{igkey}</span>:{props.ingredients[igkey]}</li>
-  })
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>A burger with the following ingredients</p>
-      <ul>
-        {ingredientsSummary}
-      </ul>
-      <p><strong>Total : </strong>{props.price.toFixed(2)}</p>
-      <div>
-        <Button clicked={props.purchaseCanceled} btnType="Danger">Cancel</Button>
-        <Button clicked={props.purchaseCanceled} btnType="Success">Continue</Button>
-      </div>
-    </Aux>
-  )
+class OrderSummary extends Component {
+  render() {
+    const ingredientsSummary = Object.keys(this.props.ingredients).map(igkey => {
+      return <li key={igkey}><span style={{ textTransform: 'capitalize' }}>{igkey}</span>:{this.props.ingredients[igkey]}</li>
+    })
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>A burger with the following ingredients</p>
+        <ul>
+          {ingredientsSummary}
+        </ul>
+        <p><strong>Total : </strong>{this.props.price.toFixed(2)}</p>
+        <div>
+          <Button clicked={this.props.purchaseCanceled} btnType="Danger">Cancel</Button>
+          <Button clicked={this.props.purchaseCanceled} btnType="Success">Continue</Button>
+        </div>
+      </Aux >
+    )
+  }
 }
 
-export default orderSummary;
+export default OrderSummary;
