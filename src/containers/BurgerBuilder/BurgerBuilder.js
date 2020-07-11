a050acab2dcfd1e6a77e3295cb4e2ab294311fc9
amans199 <ahmed.ouda1997@gmail.com>
2020-07-11 14:41:03 +0200

    #v206 add ingredients to the url and get it in the checkout component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -91,7 +91,15 @@ class BurgerBuilder extends Component {
     //     this.setState({ loading: false, purchasing: false })
     //   }, 100);
     // })
-    this.props.history.push("./checkout")
+    const queryParams = []
+    for (let i in this.state.ingredients) {
+      queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]))
+    }
+    const queryString = queryParams.join('&')
+    this.props.history.push({
+      pathname: '/checkout',
+      search: '?' + queryString
+    })
   }
   componentDidMount() {
     axios.get('https://burger-builder-amans199.firebaseio.com/ingredients.json').then(res => {
