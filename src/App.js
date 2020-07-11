98260ce78bfb21debaf591e6d97f7d2d6da985b0
amans199 <ahmed.ouda1997@gmail.com>
2020-07-11 12:44:07 +0200

    #v203 => setting up the checkout page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
-// import TheSimplestBreadcrumb from 'the-simplest-breadcrumb'
+// import Checkout from './containers/Checkout/Checkout'
+
 class App extends Component {
   render() {
     return (
       <div>
-        {/* <TheSimplestBreadcrumb /> */}
         <Layout>
           <BurgerBuilder />
         </Layout>
