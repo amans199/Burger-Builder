6e9e8f39078990a13020091fbf5c1ecccda07e9d
amans199 <ahmed.ouda1997@gmail.com>
2020-08-30 02:45:42 +0200

    #280 Installing the Redux Devtools

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import reducer from './store/reducer'
-const store = createStore(reducer)
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 const app = (
   <Provider store={store}>
     <BrowserRouter>
