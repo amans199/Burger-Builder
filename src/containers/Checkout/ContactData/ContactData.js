2ea1b0b3577b8eca4d10472c9509d9c3d85089a4
amans199 <ahmed.ouda1997@gmail.com>
2020-09-04 20:22:15 +0200

    #287 Adding Order Actions

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -117,13 +117,7 @@ class ContactData extends Component {
       orderData: formData
     }
     this.setState({ loading: false })
-    axios.post('/orders.json', order).then(res => {
-      this.setState({ loading: false })
-      this.props.history.push('/')
-    }
-    ).catch(err => {
-      console.log(err)
-    })
+
   }
 
   inputChangedHandler = (event, inputIdentifier) => {
