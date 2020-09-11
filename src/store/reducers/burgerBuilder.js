12c8829ce8cb11ac6e092bdcb053e20a9b356f4c
amans199 <ahmed.ouda1997@gmail.com>
2020-09-11 04:44:14 +0200

    #297 Refactoring Reducers

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -1,5 +1,5 @@
 import * as actionTypes from '../actions/actionTypes'
-
+import { updatedObject } from '../utility'
 const initialState = {
   ingredients: null,
   totalPrice: 4,
@@ -16,14 +16,14 @@ const INGREDIENT_PRICES = {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.ADD_INGREDIENT:
-      return {
-        ...state,
-        ingredients: {
-          ...state.ingredients,
-          [action.ingredientName]: state.ingredients[action.ingredientName] + 1
-        },
+      // this just another structured way to create the object 
+      const updatedIngredient = { [action.ingredientName]: state.ingredients[action.ingredientName] + 1 }
+      const updatedIngredients = updatedObject(state.ingredients, updatedIngredient)
+      const updatedState = {
+        ingredients: updatedIngredients,
         totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
       }
+      return updatedObject(state, updatedState)
     case actionTypes.REMOVE_INGREDIENT:
       return {
         ...state,
